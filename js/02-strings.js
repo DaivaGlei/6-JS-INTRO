26f58120a088fc7b4c839d3e7d5e96a5eca7c6d0
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-10 18:15:23 +0200

    if corection

--- js/02-strings.js
+++ js/02-strings.js
@@ -4,6 +4,9 @@ visos kombinacijos darbui su tekstu
 //Siandiena yra: kovo desimta.
 const menuo = 'Kovas';
 const diena = 'desimta';
-
 const dayString = 'Siandiena yra: ' + menuo + ' ' + diena + ' . ';
 console.log(dayString);
+
+const petrasBacktick = `Mano vardas: Petras.`;
+console.log(petrasBacktick);
+
