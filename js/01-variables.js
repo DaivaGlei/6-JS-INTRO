c0bff04fea7e4507403cda3acabcddc9f02c533e
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-10 10:57:26 +0200

    string add

---
+++ js/01-variables.js
@@ -0,0 +1,10 @@
+
+
+const x = 'wwfwfwf';
+console.log(x);
+
+const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
+console.log(abc);
+
+
+
---
+++ js/02-strings.js
@@ -0,0 +1,9 @@
+/* 
+visos kombinacijos darbui su tekstu
+*/
+//Siandiena yra: kovo desimta.
+const menuo = 'Kovas';
+const diena = 'desimta';
+
+const dayString = 'Siandiena yra: ' + menuo + ' ' + diena + ' . ';
+console.log(dayString);
