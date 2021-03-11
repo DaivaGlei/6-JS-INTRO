aa3ff6a7d6583fce5098b7a2ba34cb3d58e228a2
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-11 19:33:55 +0200

    kintamuju veiksmai

--- js/01-variables.js
+++ js/01-variables.js
@@ -4,6 +4,8 @@ const pirmasskaicius = 1;
 const antrasskaicius = 2;
 const treciasskaicius = 3;
 console.log(pirmasskaicius, antrasskaicius, treciasskaicius);
+const suma =pirmasskaicius + antrasskaicius + treciasskaicius;
+console.log(suma);
  
 const pirmaszodis = 'as';
 const antraszodis = 'esu';
@@ -14,14 +16,18 @@ const pirmassarasas = [1, 2, 3, 4, 5];
 const antrasarasas = [6, 7, 8, 9, 10];
 const treciassarasas = [11, 12, 13, 14, 15];
 console.log(pirmassarasas, antrasarasas, treciassarasas);
+const aritmetika1 = pirmassarasas[0] - pirmassarasas [1] + pirmassarasas[2] - pirmassarasas[3] + pirmassarasas[4];
+console.log(aritmetika1);
+const aritmetika2 = antrasarasas[0] - antrasarasas [1] + antrasarasas [2] - antrasarasas[3] + antrasarasas [4];
+console.log(aritmetika2);
+const aritmetika3 = treciassarasas [0] -treciassarasas [1] + treciassarasas[2] - treciassarasas[3] + treciassarasas[4];
+console.log(aritmetika3);
+
 
 const pirmagrupe =['vienas', 'du', 'trys', 'keturi', 'penki'];
 const antragrupe = ['sesi', 'septyni', 'astuoni', 'devyni', 'desimt'];
 const treciagrupe = ['vienuolika', 'dvylika', 'trylika', 'keturiolika', 'penkiolika'];
 console.log(pirmagrupe, antragrupe, treciagrupe);
-
-/* veiksmai su kintamaisiais*/
-
 
 
 
