2670a8a6aa43221c1f2c9c45bf0f9edded4ba069
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-11 18:39:18 +0200

    kintamojo iniciav

---
+++ js/01-variables.js
@@ -0,0 +1,27 @@
+/* kintamojo iniciavimas*/
+
+const pirmasskaicius = 1;
+const antrasskaicius = 2;
+const treciasskaicius = 3;
+console.log(pirmasskaicius, antrasskaicius, treciasskaicius);
+ 
+const pirmaszodis = 'as';
+const antraszodis = 'esu';
+const treciaszodis = 'lietuve';
+console.log(pirmaszodis, antraszodis, treciaszodis);
+
+const pirmassarasas = [1, 2, 3, 4, 5];
+const antrasarasas = [6, 7, 8, 9, 10];
+const treciassarasas = [11, 12, 13, 14, 15];
+console.log(pirmassarasas, antrasarasas, treciassarasas);
+
+const pirmagrupe =['vienas', 'du', 'trys', 'keturi', 'penki'];
+const antragrupe = ['sesi', 'septyni', 'astuoni', 'devyni', 'desimt'];
+const treciagrupe = ['vienuolika', 'dvylika', 'trylika', 'keturiolika', 'penkiolika'];
+console.log(pirmagrupe, antragrupe, treciagrupe);
+
+/* veiksmai su kintamaisiais*/
+
+
+
+
