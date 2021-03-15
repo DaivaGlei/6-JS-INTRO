36e4c362545d693ca0d413a9add66fd2ff2daebe
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-15 13:20:13 +0200

    for commit

---
+++ js/05-math.js
@@ -0,0 +1,10 @@
+const marks = [10, 8, 6, 4];
+
+const sum = 0;
+constcount = marks.length;
+let sum = 0;
+
+sum = sum + marks[0];
+
+const average = suma / count;
+console.log(average);
---
+++ js/06-for.js
@@ -0,0 +1,13 @@
+for (let i = 3; i <= 7; i++) {
+    console.log(i);
+}
+console.log('ciklo pabaiga');
+
+const abc = ['a', 'b', 'c', 'd'];
+
+for ( let i= 0; i<abc.length; i++ ) {
+    const raide = abc[i];
+
+    console.log(raide);
+}
+
