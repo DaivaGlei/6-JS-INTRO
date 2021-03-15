4ee3c79ad0eee68132fe924717d458becd9e135b
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-15 21:13:04 +0200

    nd commit

--- js/06-for.js
+++ js/06-for.js
@@ -11,3 +11,21 @@ for ( let i= 0; i<abc.length; i++ ) {
     console.log(raide);
 }
 
+/* ciklo namu darbai */
+
+for (let ind = 0; ind = 0; ind++) {
+    console.log(ind);
+}
+console.log('ciklo pabaiga');
+
+console.log(-------);
+
+const nd = ['0', '1', '2', '3', '4'];
+const ask = nd.length;
+console.log(nd.length); 
+
+for ( let i= 0; nd<nd.length; nd++ ) {
+    const nd1 = nd[i];
+
+    console.log(nd1);
+}
