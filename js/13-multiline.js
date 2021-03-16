b7c1554a0fc41b232ac7254656704fad27cf1cbe
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-16 22:21:42 +0200

    multiline commit

--- js/13-multiline.js
+++ js/13-multiline.js
@@ -1,6 +1,9 @@
-funcija daugyba (a, b) {
-    const rez = a * b;
+function daugyba (a, b, c) {
+    const rez = a * b * c;
     return rez;
 }
 
-console.log(daugyba(4, 5));
+console.log(daugyba(4, 5, 5));
+console.log(daugyba(5, 5, 6));
+console.log(daugyba(9, 5, 5));
+console.log(daugyba(7, 5, 10));
