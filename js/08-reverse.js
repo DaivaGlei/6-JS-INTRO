519e4cc1b7e4ab1683f7ada7109346575b3cb951
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-16 21:07:58 +0200

    reverse

--- js/08-reverse.js
+++ js/08-reverse.js
@@ -13,9 +13,16 @@ console.log(text, '->', reverse);
 for (let i = 0; i < text.length; i++) {
     reverse += text[text.length - 1 -i];
     }
+    
+    for (let i = 0; i < text.length; i++) {
+        reverse = text[i] + reverse;
+            }
 
-for (let i = 0; i < text.length; i++) {
-    reverse = text[i] + reverse;
-        }
 
-        
+  const text1 = 'labas';
+  let reverse1 ='';
+  
+  for (let i = text1.length; i > 0; i--) {
+      console.log(i, text1[i - 1]);
+      reverse =+ text1[i - 1];
+  }
