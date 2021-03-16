20eebb8e9deae451ecd927cdd309374dd99293a7
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-16 21:43:21 +0200

    liekana commit

--- js/09-liekana.js
+++ js/09-liekana.js
@@ -1,3 +1,5 @@
+            /* 0 -11 */
+
 const nuo =0;
 const iki = 11;
 const daliklis = 3;
@@ -8,6 +10,65 @@ for (let i = nuo; i <= iki; i++) {
         kiekis++;
     }
 }
-
 const ats = `Skaiciu intervale tarp ${nuo} ir ${iki}, besidalijanciu be liekanos is ${daliklis} yra ${kiekis} vienetai`;
  console.log(ats);
+
+ console.log('------is 5 dalinasi-----');
+
+ const nuo11 =0;
+const iki11 = 11;
+const daliklis11 = 5;
+let kiekis11 = 0;
+
+for (let i = nuo; i <= iki11; i++) {
+    if (i % daliklis11 === 0) {
+        kiekis11++;
+    }
+}
+const ats11 = `Skaiciu intervale tarp ${nuo11} ir ${iki11}, besidalijanciu be liekanos is ${daliklis11} yra ${kiekis11} vienetai`;
+ console.log(ats11);
+
+ console.log('--------is 7 dalinasi');
+
+ const nuo22 = 0;
+ const iki22 = 11;
+ const daliklis22 = 7;
+ let kiekis22 = 0;
+
+ for (let i = nuo22; i <= iki22; i++) {
+  if (i % daliklis22 === 0) {
+  kiekis22++;
+ }
+} 
+ const ats22 = `Skaiciu intervale tarp ${nuo22} ir ${iki22}, besidalijanciu be liekanos is ${daliklis22} yra ${kiekis22} vienetai. `
+console.log(ats22);
+
+            /* 8 -31 */
+
+const nuo1 = 8;
+const iki1 = 31;
+const daliklis1 = 3;
+let kiekis1 = 0;
+
+for (let i = nuo1; i <= iki1; i++) {
+    if (i % daliklis1 === 0) {
+        kiekis1++;
+    }
+}
+const ats1 = `Skaiciu intervalas tarp ${nuo1} ir ${iki1} , besidalijanciu be liekanos is ${daliklis1} yra ${kiekis1} vienetai.`
+console.log(ats1);
+
+            /* -18 - 18 */
+
+const nuo2 = -18;
+const iki2 = 18;
+const daliklis2 = 3;
+let kiekis2 = 0;
+
+for (let i = nuo2; i <= iki2; i++) {
+    if (i % daliklis2 ===0){
+        kiekis2++;
+        }
+    }
+    const ats2 = `Skaiciu intervalas tarp ${nuo2} ir ${iki2}, besidalijanciu be liekanos is ${daliklis2} yra ${kiekis2} vienetai`
+    console.log(ats2);
