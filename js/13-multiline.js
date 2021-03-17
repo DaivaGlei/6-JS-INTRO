7fe07f04089b422a55fde2aeedc37d068409cec3
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-17 11:02:32 +0200

    multiline commit

--- js/13-multiline.js
+++ js/13-multiline.js
@@ -1,9 +1,18 @@
-function daugyba (a, b, c) {
-    const rez = a * b * c;
+function daugyba (a, b) {
+    if (typeof a !== 'number') {
+        return 'Pirmasis parametras turi buti skaiciaus tipo';
+    }
+    if (typeof b !== 'number') {
+        return 'Antrasis skaicius turi buti skaiciaus reiksmes'
+    }
+    const rez = a * b;
     return rez;
 }
 
-console.log(daugyba(4, 5, 5));
-console.log(daugyba(5, 5, 6));
-console.log(daugyba(9, 5, 5));
-console.log(daugyba(7, 5, 10));
+console.log(daugyba(4, 5), '->', 20);
+console.log(daugyba(5, 5), '->', 25);
+console.log(daugyba(9, 5), '->', 45);
+console.log(daugyba(7, 5), '->', 35);
+
+console.log(daugyba('trys', 'penki'), '->', 15);
+console.log(daugyba(3, 'penki'), '->', 15);
