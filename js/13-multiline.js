e80ce299c70884e7632465236a890422ee3e3010
DaivaGlei <79476239+DaivaGlei@users.noreply.github.com>
2021-03-17 20:51:09 +0200

    multiline commit

--- js/13-multiline.js
+++ js/13-multiline.js
@@ -29,3 +29,56 @@ console.log(daugyba(7, 5), '->', 35);
 
 console.log(daugyba('trys', 'penki'), '->', 15);
 console.log(daugyba(3, 'penki'), '->', 15);
+
+/* Daugybos nd
+Funkcija pavadinimu “daugyba”:
+
+priima du skaičiaus tipo kintamuosius
+atskirame kintamajame įsimena sandaugos reikšmę
+gražina saudaugos rezultatą
+TESTAI:
+console.log( daugyba( skaicius1, skaicius2 ) );
+console.log( daugyba( skaicius3, skaicius2 ) );
+console.log( daugyba( skaicius1, skaicius3 ) );
+rezultatas: teisingos reikšmės;
+
+*/
+
+function daugyba (k, t) {
+   if (typeof k !== 'number')
+   return 'Pirmasis parametras turi buti skaiciaus tipo';
+    if (typeof t !== 'number')
+    return 'Antrasis turi buti skaiciaus tipo';
+    if (isNaN(k)) {
+        return 'Pirmasis parametras turi buti normalus skaicius';
+    } if (isNaN(t)) {
+        return 'Antras parametras turi buti normalus skaicius'; 
+    } 
+        if (k === Infinity) {
+    return 'Pirmasis parametras turi buti normalus skaicius';
+    }
+         if (t === Infinity) {
+    return 'Antras parametras turi buti normalus skaicius';
+         }
+    const daugyba = k * t;
+    return daugyba;
+}
+
+console.log(daugyba(4, 5));
+console.log(daugyba(5, 5));
+console.log(daugyba(9, 5));
+console.log(daugyba(7, 5));
+console.log(daugyba('fygyg', 5));
+console.log(daugyba(7, 'hnknk'));
+console.log(daugyba('000555', 5));
+console.log(daugyba(7, '5555'));
+console.log(daugyba('true', 5));
+console.log(daugyba(7, 'false'));
+console.log(daugyba([], 5));
+console.log(daugyba(7, []));
+console.log(daugyba(NaN, 5));
+console.log(daugyba(7, NaN));
+console.log(daugyba(Infinity, 5));
+console.log(daugyba(7, Infinity));
+console.log(daugyba(-Infinity, 5));
+console.log(daugyba(7, -Infinity));
